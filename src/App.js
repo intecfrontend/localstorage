e3b04a7b1e6dcccc0b2548967b4d66718c78762f
intecfrontend <53944321+intecfrontend@users.noreply.github.com>
2023-06-02 14:46:44 +0200

    r

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LocalStorageApp from './LocalStorageApp';
+
+const App = () => {
+  return (
+    <div>
+      <h1>My App</h1>
+      <LocalStorageApp />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/zzzz.ts
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+
+const MyComponent = () => {
+  const [inputValue1, setInputValue1] = useState('')
+  const [inputValue2, setInputValue2] = useState('')
+
+  useEffect(() => {
+    const storedValue = localStorage.getItem('inputValue2')
+    if (storedValue) {
+      setInputValue2(storedValue)
+    }
+  }, [])
+
+  const handleInputChange1 = event => {
+    setInputValue1(event.target.value)
+  }
+
+  const handleInputChange2 = event => {
+    const value = event.target.value
+    setInputValue2(value)
+    localStorage.setItem('inputValue2', value)
+  }
+
+  const handleReset = () => {
+    setInputValue1('')
+    setInputValue2('')
+    localStorage.removeItem('inputValue2')
+  }
+
+  return (
+    <div>
+      <h2>Input 1: {inputValue1}</h2>
+      <input type='text' value={inputValue1} onChange={handleInputChange1} />
+      <h2>Input 2: {inputValue2}</h2>
+      <input type='text' value={inputValue2} onChange={handleInputChange2} />
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  )
+}
+
+export default MyComponent
